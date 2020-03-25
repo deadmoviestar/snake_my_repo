b9555a994da6ac55da87577ddce2872454c43719
Dmitry Pylaev <pylaevdmitry@yandex.ru>
2020-03-25 11:15:36 +0400

    add point

---
+++ snake_my/snake_my/point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake_my
+{
+    class point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
