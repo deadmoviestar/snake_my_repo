7d0886068a0d23fabfa6742111882a12412ac6c4
Dmitry Pylaev <pylaevdmitry@yandex.ru>
2020-03-26 15:51:57 +0400

    Горизтонтальная линия

---
+++ snake_my/snake_my/HorizontalLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake_my
+{
+    class HorizontalLine
+    {
+        List<point> plist;
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            plist = new List<point>();
+
+            for(int x = xLeft; x <= xRight; x++) 
+            {
+                point p = new point(x, y, sym);
+                plist.Add(p);
+            }
+
+        }
+        public void Draw()
+        {
+            foreach(point p in plist)
+            {
+                p.draw();
+            }
+        }
+    }
+}
--- snake_my/snake_my/Program.cs
+++ snake_my/snake_my/Program.cs
@@ -7,41 +7,9 @@ namespace snake_my
     {
         static void Main(string[] args)
         {
-            point p1 = new point(1, 3, '$');
-            p1.draw();
+            HorizontalLine line = new HorizontalLine(2, 10, 3, '#');
+            line.Draw();
 
-            point p2 = new point(3, 6, '^');
-            p2.draw();
-
-            point p3 = new point(6, 6, '$');
-            p3.draw();
-
-            point p4 = new point(8, 8, '^');
-            p4.draw();
-
-            List<int> numlist = new List<int>();
-
-            numlist.Add(0);
-            numlist.Add(3);
-            numlist.Add(4);
-
-            int x = numlist[0];
-            int y = numlist[1];
-            int z = numlist[2];
-
-            foreach (int i in numlist)
-            {
-                Console.WriteLine(i);
-            }
-
-            numlist.RemoveAt(0);
-
-            List<point> pl = new List<point>();
-
-            pl.Add(p1);
-            pl.Add(p2);
-            pl.Add(p3);
-            pl.Add(p4);
             Console.ReadLine();
         }
     }
